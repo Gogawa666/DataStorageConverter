0e59a0dfbe12fc47d59f74c9a20fd7cfdfae4989
Gogawa666 <gogawa@lcs.on.ca>
2021-02-12 12:00:37 -0500

    Taking input and producing some output for converter program

--- iOS-DataStorageConverter/ContentView.swift
+++ iOS-DataStorageConverter/ContentView.swift
@@ -9,32 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     
-   @State  private var name: String = ""
-    
-    @State private var feeling: String = ""
-    
-    private var greeting: String {
-        return "Hello, \(name), you are \(feeling)"
-        
+    //Mark: Stored properties
+    private var output: String {
+        return "You typed in \(input)"
     }
+    
+    @ State private var input: String = ""
+    
     
     var body: some View {
         
         Form {
             
+            TextField("e.g.: 1824", text: $input)
+                .keyboardType(.numberPad)
             
-            TextField("Enter your name", text: $name)
-            
-            Picker("Mood", selection: $feeling) {
-                
-                Text("😄").tag("happy")
-                Text("🙂").tag("fine")
-                Text("🙁").tag("sad")
-            }
-            .pickerStyle(SegmentedPickerStyle())
-            
-            Text(greeting)
-                .padding()
+            Text(output)
+          
         }
         }
       
